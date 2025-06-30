24792d878a4b9a68124d1021d8afefe073549838
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2025-06-30 18:55:59 +1000

    init landing page build.

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://example.com",
+	integrations: [mdx(), sitemap()],
+	vite: {
+		resolve: {
+			alias: {
+				"@icons": "/src/icons",
+				"@images": "/src/images",
+				"@components": "/src/components",
+			},
+		},
+	},
+});
