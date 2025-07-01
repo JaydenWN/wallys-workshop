1d0c3cad9d9e40f309db00d3c192d644fe0b3e84
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2025-07-01 18:24:12 +1000

    init complete build

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Mobile Mechanic Glen Innes to Inverell | Wally’s Workshop';
+export const SITE_DESCRIPTION = 'Wally’s Workshop is a trusted mobile mechanic servicing Glen Innes, Deepwater, Tenterfield, Inverell and nearby towns. We come to you for logbook services, general repairs, breakdowns, and maintenance on cars, bikes, trailers and farm equipment. With honest advice, fair pricing and reliable service, Wally’s Workshop keeps locals moving.';
